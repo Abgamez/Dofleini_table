dca5f35fc8703b4192f42cdde5dd77b02559e9eb
Abgamez <abgamez@icloud.com>
2023-11-11 09:21:33 -0500

    Terminado el Table Dofleini

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Table from './pages/table';
-import Tables from './pages/Tabla_Page';
+import Table from './pages/table';;
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Table/>} />
-        <Route path='/q' element={<Tables/>} />
       </Routes>
     </Router>
   );
